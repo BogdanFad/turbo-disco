05cc31ed71317f67427c0f030ea5ff605cf3ecaf
Bogdan <bogdan.fad@yandex.ru>
2022-12-28 21:33:31 +0300

    task 50 complete

---
+++ Task50/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает число, и возвращает индексы 
+// числа в двумерном массиве или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+Console.WriteLine("Введите число ");
+int number = int.Parse(Console.ReadLine()!);
+int [, ] array = new int [3, 4];
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i,j] = new Random().Next(0,10);
+        Console.Write(array[i,j] + " ");
+    }
+Console.WriteLine();
+}
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        if (array[i, j] == number)
+        {
+            Console.WriteLine($"ячейка {i},{j}");
+        }
+        else
+        {
+            Console.WriteLine($"{number} -> такого числа в массиве нет ");
+        }
+    }
+}
